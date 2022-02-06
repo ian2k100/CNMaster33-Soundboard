313a629150a02f6154bb55aa402106937114ebb4
ian2k100 <ianrmukoma@gmail.com>
2022-02-06 05:11:24 +0000

    Forgot which form this is

--- soundboard.js
+++ soundboard.js
@@ -3,3 +3,10 @@ function play(sound){
     audio.setAttribute('src', sound);
     audio.play();
 }
+
+window.addEventListener('keydown', function (event) {
+    if (event.key == 'd') {
+        document.getElementById("audio").play('sounds/blockhit.wav');
+        audio.play();
+    }
+  });
